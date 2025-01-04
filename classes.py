2bb456bbf09a22ddafd6ba58b215f2c0b199ef45
Andrew-Proskura <andrewproskura3@gmail.com>
2025-01-04 19:16:21 +0300

    Добавлен класс Weapon и переработан класс Player

--- classes.py
+++ classes.py
@@ -34,6 +34,8 @@ class Creature():
 
 
 class Enemy(Creature):
+    def appera(self, pos):
+        pass
 
     def get_path(self):
         pass
@@ -46,10 +48,57 @@ class Enemy(Creature):
     #Атака
 
 class Player(Creature):
+    def __init__(self, type):
+        super().__init__(type)
+        self.health = type
+        self.speed = 2
+        self.first_weapons = 0
+        self.second_weapons = 0
+        self.third_weapons = 0
+        self.current_weapon = 1
+        self.direction = 1
+        self.attacking_weapons = []
+
+    def set_current(self, type):
+        self.current_weapon = type
+    #Меняем оружие
+
     def do_attack(self):
-        pass
+        self.attacking_weapons.append(Weapon(flying=True, type=self.current_weapon, direction=self.direction))
     #Атака
 
     def take_weapon(self):
         pass
     #Берём оружие
+
+    def take_cherry(self):
+        if self.health < 10:
+            self.health += 1
+        else:
+            pass
+    #берём вишенку
+    def interaction(self):
+        pass
+    #взаимодействие
+
+class Weapon():
+    def __init__(self, type, direction, flying):
+        self.type = type
+        self.damage = type
+        self.range = type
+        self.direction = direction
+        self.flying = flying
+        self.visible = True
+        self.cells_to_fly = type
+
+    def do_fly(self, direction):
+        pass
+
+    def check_hit_enemy(self):
+        pass
+
+    def check_hit_wall(self):
+        pass
+
+    def appear(self, pos):
+        pass
