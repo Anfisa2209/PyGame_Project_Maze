ea51f7b579dd2355d788a382f1d7dfeccec40527
Andrew-Proskura <andrewproskura3@gmail.com>
2025-01-05 22:06:08 +0300

    Переработан класс Creature - он наследует от класса Sprite для удобства. Функция move переделана под Sprite. Также небольшие изменения претерпели функция do_die, классы Player и Weapon.

--- classes.py
+++ classes.py
@@ -1,17 +1,40 @@
-class Creature():
+import os
+import sys
 
-    def __init__(self, type):
-        super.__init__(type)
+import pygame
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Creature(pygame.sprite.Sprite):
+
+    def __init__(self, type, *group, pic_name, pos):
+        super().__init__(type, *group, pic_name, pos)
+        self.image = load_image(pic_name)
         self.health = type
         self.speed = 2
-        #Параметры здоровья, и скорости
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
+        self.is_alive = True
+        #Параметры здоровья, скорости, положения, картинка и проверка на то, что существо живо. Creature будет наследником pygame.sprite.Sprite, чтобы было удобнее передвигаться и ставить картинки
 
-    def get_pos(self):
-        pass
-        # В этой функции берём позицию существа и пихаем в кортеж
 
     def do_die(self):
-        pass
+        self.is_alive = False
     # В этой функции существо умирает и удаляестя с поля(опционально - анимация смерти)
 
     def get_hit(self, damage):
@@ -21,21 +44,18 @@ class Creature():
         #Получение урона, если хп меньше либо равно нулю, то смерть
 
     def move(self, direction):
-        x, y = self.get_pos()
         if direction == 'up':
-            y -= self.speed
+            self.rect.y -= self.speed
         elif direction == 'down':
-            y += self.speed
+            self.rect.y += self.speed
         if direction == 'left':
-            x -= self.speed
+            self.rect.x -= self.speed
         elif direction == 'right':
-            x += self.speed
+            self.rect.x += self.speed
     # Собственно, движение
 
 
 class Enemy(Creature):
-    def appera(self, pos):
-        pass
 
     def get_path(self):
         pass
@@ -48,9 +68,13 @@ class Enemy(Creature):
     #Атака
 
 class Player(Creature):
-    def __init__(self, type):
-        super().__init__(type)
-        self.health = type
+    def __init__(self, type, *group, pic_name, pos):
+        super().__init__(type, *group, pic_name, pos)
+        self.image = load_image(pic_name)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
+        self.is_alive = True
+        self.health = 3
         self.speed = 2
         self.first_weapons = 0
         self.second_weapons = 0
@@ -81,8 +105,12 @@ class Player(Creature):
         pass
     #взаимодействие
 
-class Weapon():
-    def __init__(self, type, direction, flying):
+class Weapon(pygame.sprite.Sprite):
+    def __init__(self, type, *group, pic_name, pos, direction, flying):
+        super().__init__(type, *group, pic_name, pos, direction, flying)
+        self.image = load_image(pic_name)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
         self.type = type
         self.damage = type
         self.range = type
@@ -90,6 +118,7 @@ class Weapon():
         self.flying = flying
         self.visible = True
         self.cells_to_fly = type
+        #Параметр type - тип оружия, flying - летит ли оружие(летит - когда кинул игрок, не летит - когда лежит на земле), visible - то же самое, что и is_alive у Creature, cells_to_fly - кол-во клеток, которое ещё может пролететь оружие
 
     def do_fly(self, direction):
         pass
@@ -98,7 +127,4 @@ class Weapon():
         pass
 
     def check_hit_wall(self):
-        pass
-
-    def appear(self, pos):
         pass
